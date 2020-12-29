5260d73bd6b76faf9561fec8cd3755eaffdd6bdb
capeieboi <capeieboi@gmail.com>
2020-12-29 15:30:18 +0530

    All done, heroku left

--- test_flak.py
+++ test_flak.py
@@ -8,10 +8,15 @@ import joblib
 app = Flask(__name__)
 app.secret_key = 'dljsaklqk24e21cjn!Ew@@dsa5'
 
-model = joblib.load('/home/capeie/covid_severity_predictor/models/rfc_pipeline_hospitalization.pkl')
-
+hosp_model = joblib.load('/home/capeie/covid_severity_predictor/models/rfc_pipeline_hospitalization.pkl')
+icu_model = joblib.load('/home/capeie/covid_severity_predictor/models/rfc_pipeline_ICU.pkl')
+ventilator_model = joblib.load('/home/capeie/covid_severity_predictor/models/rfc_pipeline_Ventilator.pkl')
+death_model = joblib.load('/home/capeie/covid_severity_predictor/models/rfc_pipeline_Death.pkl')
 prediction_out = 'Ambulatory'
-prediction_translate = {1:'Ambulatory',2:'Hospitalized'}
+prediction_translate = {1:'Can self quarantine',2:'Will need Hospitalization',99:'Not sure'}
+general = {0:'Chill no ded ',1:'Rip he ded af'}
+icu_translate = {1:'ICU needed',2:"ICU not needed"}
+vet_translate = {1:'Ventilator needed',2:"Ventilator not needed"}
 @app.route('/',methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -30,8 +35,27 @@ def index():
 def skrtskrt():
     input_dict = session['req']
     df = pd.DataFrame(input_dict,index=[0])
-    output = model.predict(df)
-    prediction_out = prediction_translate[output[0]]
-    return prediction_out
+    hosp_output = hosp_model.predict(df)
+    hosp_output = prediction_translate[hosp_output[0]]
+    death_output = death_model.predict(df)
+    print(death_output)
+    prediction_out = general[death_output[0]]
+    icu_output = 'Not Applicable'
+    ventilator_output = 'Not Applicable'
+    if hosp_output == 'Will need Hospitalization':
+        icu_output = icu_model.predict(df)
+        icu_output = icu_translate[icu_output[0]]
+        ventilator_output = ventilator_model.predict(df)
+        ventilator_output = vet_translate[vent_output[0]]
+
+    return render_template('predictions.html',hosp=hosp_output,ded=prediction_out,icu=icu_output,ventilator=ventilator_output)
+    '''if hosp_output[0] == 2:
+        icu_output = icu_model.predict(df)
+        vent_output = ventilator_model.predict(df)
+        lit = (hosp_output,death_output,icu_output,vent_output)
+        return lit
+    else:
+        lit = (hosp_output,death_output)
+        return lit'''
 if __name__=="__main__":
     app.run(debug=True)
