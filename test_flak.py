5fc09c2d62fd5dbe51c8e232acb773f9c9303c2c
capeieboi <capeieboi@gmail.com>
2020-12-29 11:20:52 +0530

    Flask form done

--- test_flak.py
+++ test_flak.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=["GET", "POST"])
 def index():
-    return 'Hello, World!'
+    if request.method == "POST":
+
+        req = request.form.to_dict()
+        print(req)
+
+        return redirect(request.url)
+
+    return render_template("form.html")
 if __name__=="__main__":
     app.run(debug=True)
